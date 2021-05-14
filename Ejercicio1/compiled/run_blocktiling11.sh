bcce49bfad946f7c0ba8e3deb8c292434587970e
Amaru <amaruescalante@gmail.com>
2021-05-14 16:18:59 -0500

    fix typo in .sh files

--- Ejercicio1/compiled/run_blocktiling11.sh
+++ Ejercicio1/compiled/run_blocktiling11.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J serial_transpuesto_2048 # nombre del job
+#SBATCH -J serial_blocktiling_2048 # nombre del job
 #SBATCH -N 1 # numero de nodos
 #SBATCH --tasks-per-node=1 # numero de tasks por nodo
-#SBATCH --output=log_transpuesto_2048 # numero de tasks por nodo
+#SBATCH --output=log_blocktiling_2048 # numero de tasks por nodo
 
 module load gcc/5.5.0
-srun serial_transpuesto_2048 # siendo prueba_mpi el nombre del programa mpi
+srun serial_blocktiling_2048 # siendo prueba_mpi el nombre del programa mpi
 module unload gcc/5.5.0
--- Ejercicio1/compiled/run_blocktiling12.sh
+++ Ejercicio1/compiled/run_blocktiling12.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J serial_transpuesto_4096 # nombre del job
+#SBATCH -J serial_blocktiling_4096 # nombre del job
 #SBATCH -N 1 # numero de nodos
 #SBATCH --tasks-per-node=1 # numero de tasks por nodo
-#SBATCH --output=log_transpuesto_4096 # numero de tasks por nodo
+#SBATCH --output=log_blocktiling_4096 # numero de tasks por nodo
 
 module load gcc/5.5.0
-srun serial_transpuesto_4096 # siendo prueba_mpi el nombre del programa mpi
+srun serial_blocktiling_4096 # siendo prueba_mpi el nombre del programa mpi
 module unload gcc/5.5.0
